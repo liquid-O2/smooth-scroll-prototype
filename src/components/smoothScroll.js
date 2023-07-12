734ca89c60505877f68bc9c5daae45c17b3cbb5f
liquid-O2 <this.is.arunava.b@gmail.com>
2023-07-12 12:09:58 +0530

    Cleanup

--- src/components/smoothScroll.js
+++ src/components/smoothScroll.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react"
+import { useEffect } from "react"
 import Lenis from "@studio-freight/lenis"
 
-export default function SmoothScroll({ children }) {
+export default function SmoothScroll() {
 	useEffect(() => {
 		const lenis = new Lenis({ duration: 1.2 })
 		function raf(time = 200) {
