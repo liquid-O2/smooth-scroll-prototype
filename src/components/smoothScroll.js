b2b0308686f430895387788e7c5937b9a34847df
liquid-O2 <this.is.arunava.b@gmail.com>
2023-07-12 15:36:41 +0530

    Change to smoothScroll

--- src/components/smoothScroll.js
+++ src/components/smoothScroll.js
@@ -1,14 +1,21 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import Lenis from "@studio-freight/lenis"
 
 export default function SmoothScroll() {
+	const [isWindow, setIsWindow] = useState(false)
 	useEffect(() => {
-		const lenis = new Lenis({  duration: 1.2, smoothWheel: true })
+		setIsWindow(true)
+		const lenis = new Lenis({ duration: 1.2, smoothWheel: true })
 		function raf(time) {
 			lenis.raf(time)
 			requestAnimationFrame(raf)
 		}
 		requestAnimationFrame(raf)
-	}, [])
+
+		return () => {
+			lenis.destroy()
+			setIsWindow(false)
+		}
+	}, [isWindow])
 	return null
 }
