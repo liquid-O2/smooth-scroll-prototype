e80032f3f30d9d195aed57c4c8e6405aa32a7c2c
liquid-O2 <this.is.arunava.b@gmail.com>
2023-07-12 15:29:52 +0530

    ---

--- src/components/smoothScroll.js
+++ src/components/smoothScroll.js
@@ -3,8 +3,7 @@ import Lenis from "@studio-freight/lenis"
 
 export default function SmoothScroll() {
 	useEffect(() => {
-		const body = document.body
-		const lenis = new Lenis({ wrapper: body, duration: 1.2, smoothWheel: true })
+		const lenis = new Lenis({  duration: 1.2, smoothWheel: true })
 		function raf(time) {
 			lenis.raf(time)
 			requestAnimationFrame(raf)
